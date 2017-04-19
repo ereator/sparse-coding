062df38289278cf0f111b1b3aacea816b4daaadd
Sergey Kosov <sergey.kosov@project-10.de>
2017-04-19 08:43:37 +0200

    skeleton is set up

--- source/sc_test.cpp
+++ source/sc_test.cpp
@@ -1,4 +1,21 @@
-int main()
+#include "DGM.h"
+#include "FEX.h"
+#include "FEX\LinearMapper.h"
+#include "VIS.h"
+
+namespace dgm = DirectGraphicalModels;
+
+void print_help(void)
 {
+	printf("Usage: \"sc_test.exe\" input_dict.dic input_file.txt output_file.txt\n");
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4) {
+		print_help();
+		return 0;
+	}
+	
 	return 0;
 }
--- source/sc_train.cpp
+++ source/sc_train.cpp
@@ -1,11 +1,29 @@
 #include "DGM.h"
 #include "FEX.h"
+#include "FEX\LinearMapper.h"
 #include "VIS.h"
 
 namespace dgm = DirectGraphicalModels;
 
-int main()
+void print_help(void)
 {
+	printf("Usage: \"sc_train.exe\" folder_with_train_data width step nWords sc_params output_dict\n");
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 7) {
+		print_help();
+		return 0;
+	}
+
+	std::string srcFolder = argv[1];
+	// find all txt file inside the folder
+
+	int res = dgm::fex::linear_mapper<int>(25, 0, 100);
+	printf("res = %d\n", res);
+	getchar();
+
 	dgm::CGraphExt *pgraph = new dgm::CGraphExt(5);
 	pgraph->build(cvSize(10, 10));
 	dgm::vis::showGraph3D(500, pgraph, [](size_t n) {return Vec3f( static_cast<float>(n / 10), static_cast<float>(n % 10), 0); });
