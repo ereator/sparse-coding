07416826d8f27a97f5e7abc71c4db34e5e02a602
Sergey Kosov <sergey.kosov@project-10.de>
2017-04-26 19:55:37 +0200

    sc_train is ready

--- source/sc_train.cpp
+++ source/sc_train.cpp
@@ -3,13 +3,21 @@
 #include "FEX.h"
 #include "FEX\LinearMapper.h"
 #include "DGM\parallel.h"
-#include "VIS.h"
 
 namespace dgm = DirectGraphicalModels;
 
 void print_help(void)
 {
-	printf("Usage: \"sc_train.exe\" input_folder width step nWords sc_params output_dict\n");
+	printf("Usage: \"sc_train.exe\" \n");		// 0
+	printf("\t <input folder> \n");				// 1
+	printf("\t <number of sequences> \n");		// 2
+	printf("\t <window width> \n");				// 3
+	printf("\t <window step> \n");				// 4
+	printf("\t <num words> \n");				// 5
+	printf("\t <batch> \n");					// 6
+	printf("\t <number of iteration> \n");		// 7
+	printf("\t <learning rate> \n");			// 8
+	printf("\t <output dictionary> \n");		// 9
 }
 
 vec_string_t findFilesInDirectory(std::string &directory)
@@ -38,34 +46,36 @@ vec_string_t findFilesInDirectory(std::string &directory)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
+	if (argc != 10) {
 		print_help();
 		return 0;
 	}
 
 	// Parsing Parameters
 	std::string		srcFolder	= argv[1];
-	const size_t	nSequences	= 6;
-	size_t 			windowSize	= 16;
-	word			step		= 1;
-	word			nWords		= 512;
-	dword			batch		= 2000;
-	unsigned int	nIt			= 100;
-	float			lRate		= 1e-2f;
-	std::string		outDicFile	= "dictionary.dic";
+	size_t			nSequences	= atoi(argv[2]);			// 6
+	size_t			windowSize  = atoi(argv[3]);			// 16
+	word			windowStep	= atoi(argv[4]);			// 1
+	word			nWords		= atoi(argv[5]);			// 512
+	dword			batch		= atoi(argv[6]); 			// 2000
+	int				nIt			= atoi(argv[7]);			// 1000
+	float			lRate		= (float) atof(argv[8]);	// 1e-2f
+	std::string		outDicFile	= argv[9];
 	
-	const int		sampleLen	= windowSize * nSequences;
-	printf("sample len = %d\n", sampleLen);
-
+	// Additional constants
+	const bool		rangeShow	= true;
+	const float		rangeMin	= -16384;
+	const float		rangeMax	=  16383;
+	const int		sampleLen	=  nSequences * windowSize;
+	printf("Sample length = %d\n", sampleLen);
 
 	// Reading the Data
 	vec_string_t vFiles = findFilesInDirectory(srcFolder);
 	
-	Mat		X;
-	Mat		sample(1, sampleLen, CV_16UC1);
-	bool	estimate_ranges = true;
-	float	min_el[nSequences];	memset(min_el, 0, nSequences * sizeof(float));
-	float	max_el[nSequences]; memset(max_el, 0, nSequences * sizeof(float));
+	Mat			X;
+	Mat			sample(1, sampleLen, CV_16UC1);
+	vec_float_t	min_el(nSequences, 0.0f);	
+	vec_float_t max_el(nSequences, 0.0f); 
 	for (auto file : vFiles) {
 		printf("Processing file %s\n", file.c_str());
 
@@ -84,7 +94,7 @@ int main(int argc, char *argv[])
 		DGM_ASSERT(vData.size() % nSequences == 0);
 		size_t sequenceLen = vData.size() / nSequences;
 		
-		if (estimate_ranges) {	// Check min and max values
+		if (rangeShow) {	// Check min and max values
 			for (size_t s = 0; s < nSequences; s++) {		// sequences
 				size_t offset = s * sequenceLen;
 				for (size_t i = 0; i < sequenceLen; i++) {	// elements
@@ -95,17 +105,17 @@ int main(int argc, char *argv[])
 			} // s
 		} 
 		
-		for (size_t i = 0; i < sequenceLen - windowSize; i++) {		// i = start of the window
-			for (size_t s = 0; s < nSequences; s++) 				// sequences
+		for (size_t i = 0; i < sequenceLen - windowSize; i += windowStep) {		// i = start of the window
+			for (size_t s = 0; s < nSequences; s++) 							// sequences
 				for (size_t j = 0; j < windowSize; j++) {
 					float el = vData[i + s * sequenceLen + j];
-					sample.at<word>(0, s * windowSize + j) = dgm::fex::linear_mapper<word>(el, -16384, 16383);
+					sample.at<word>(0, s * windowSize + j) = dgm::fex::linear_mapper<word>(el, rangeMin, rangeMax);
 				}
 			X.push_back(sample);
 		} // i
 	}
-	if (estimate_ranges) {
-		printf("\nThe value ranges for %ld sequences (if estimated):\n", nSequences);
+	if (rangeShow) {
+		printf("\nThe value ranges for %ld sequences:\n", nSequences);
 		for (size_t s = 0; s < nSequences; s++)
 			printf("seq[%d] \\in [%.2f; %.2f]\n", s, min_el[s], max_el[s]);
 	}
